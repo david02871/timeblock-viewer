ece65e68ebd14c2835fbe4fbedda74f08cf7e9af
David Young <david.young@brightminded.com>
2020-02-10 17:40:00 +0000

    include in-progress time entries

--- src/clockify.js
+++ src/clockify.js
@@ -7,10 +7,13 @@ export async function getTimeBlocks() {
 
     let timeBlocks = timeEntriesList.map(tb => {
         const project = projects.find(p => p.id === tb.projectId);
+        const start = Date.parse(tb.timeInterval.start);
+        // in-progress timeblocks have an end date of null which we set to the current time
+        const end = Date.parse(tb.timeInterval.end) || new Date().getTime();
 
         return {
-            start: Date.parse(tb.timeInterval.start),
-            end: Date.parse(tb.timeInterval.end),
+            start,
+            end,
             name: project ? project.name : 'No Project',
             color: project ? project.color : 'darkgrey'
         }
