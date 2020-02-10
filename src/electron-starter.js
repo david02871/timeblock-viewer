628a459e8d507e66f07d0454969d2954b1f2310e
David Young <david.young@brightminded.com>
2020-02-10 17:39:24 +0000

    include electron

---
+++ src/electron-starter.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 50, height: 500});
+    mainWindow.loadURL('http://localhost:5000');
+    mainWindow.webContents.openDevTools({ mode: 'detach' });
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
