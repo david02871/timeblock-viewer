6a2c79b194d2b0cae6cf1c276d7959a107c919b4
David Young <david.young@brightminded.com>
2020-02-12 19:16:13 +0000

    Use reactive labels & spread props

--- src/App.svelte
+++ src/App.svelte
@@ -3,80 +3,54 @@
 	import TimeBlock from './TimeBlock.svelte';
 	import { onDestroy } from 'svelte';
 
+	let height = window.innerHeight;
 	let timeEntries = [];
 	let gaps = [];
 
 	let today = new Date();
-	let todayMs = today.getTime();
-	let startTimeMs = today.setHours(9, 0, 0, 0); // 9:00am
-	let endTimeMs = today.setHours(18, 0, 0, 0); // 6:00pm
+	$: todayMs = today.getTime();
+	$: startTimeMs = today.setHours(9, 0, 0, 0); // 9:00am
+	$: endTimeMs = today.setHours(18, 0, 0, 0); // 6:00pm
 
 	let fetchPollSeconds = 60000; // fetch new timeblocks every minute
 	let timePollSeconds = 10000; // update cursor time every 10 seconds
 
-	const updateCursor = () => {
-		today = new Date();
-		todayMs = today.getTime();
-	}
+	$: gaps = timeEntries
+		.map((e, i) => ({ start: i && timeEntries[i - 1].end, end: e.start })) // gaps between entries
+		.filter(e => (e.end > startTimeMs) && (e.end - e.start > 0)); // on screen only, no overlaps
 
 	const timeToPx = (timeMs) => {
-		const windowHeight = window.innerHeight;
-		const px = ((timeMs - startTimeMs) / (endTimeMs - startTimeMs)) * windowHeight;
-
-		return Math.min(px, windowHeight);
+		const px = ((timeMs - startTimeMs) / (endTimeMs - startTimeMs)) * height;
+		return Math.min(px, height);
 	}
 
-	const findGaps = () => {
-		let newGaps = [];
-		timeEntries.forEach((entry, index) => {
-			// off screen
-			const isOffScreen = timeToPx(entry.start) < 0;
-			if (index === 0 || isOffScreen) {
-				return;
-			}
-
-			const previousEntry = entry[index - 1];
-			const gap = entry.start - previousEntry.end;
-
-			// check for overlap
-			if (gap > 0) {
-				newGaps.push({
-					start: previousEntry.end,
-					end: entry.start
-				});
-			}
-		});
-
-		gapBlocks = newBlocks;
-	}
+	$: console.log(height);
 
 	const getBlocks = async () => {
 		timeEntries = await getTimeBlocks()
 			.catch(error => console.error(error));
-
-		gaps = findGaps(timeEntries);
 	}
 
 	getBlocks();
 
 	const fetchPollInterval = setInterval(getBlocks, fetchPollSeconds);
-	const timePollInterval = setInterval(updateCursor, timePollSeconds);
+	const timePollInterval = setInterval(() => today = new Date(), timePollSeconds);
 
 	onDestroy(() => {
-		clearInterval(fetchPollInterval)
-		clearInterval(timePollInterval)
+		clearInterval(fetchPollInterval);
+		clearInterval(timePollInterval);
 	});
 </script>
 
-<!-- <svelte:window on:resize={updateTimeBlocks} /> -->
+<svelte:window bind:innerHeight={height} />
 
 <main>
 	<div class='time-block-container'>
 		{#each timeEntries as t}
-			<TimeBlock start={t.start} end={t.end} color={t.color} timeToPx={timeToPx} />
+			<TimeBlock timeToPx={timeToPx} {...t} />
 		{/each}
 		{#each gaps as g}
-			<TimeBlock start={g.start} end={g.end} isGap={true} timeToPx={timeToPx} />
+			<TimeBlock isGap={true} timeToPx={timeToPx} {...g} />
 		{/each}
 		<div class='time-cursor' style='top: {timeToPx(todayMs)}px'></div>
 	</div>
@@ -91,25 +65,6 @@
 
 	main {
 		height: 100vh;
-	}
-
-	.gap-block {
-		position: absolute;
-		overflow-y: hidden;
-		left: 0px;
-		width: 100%;
-		background-color: #222;
-		cursor: pointer;
-	}
-
-	.time-block {
-		position: absolute;
-		overflow-y: hidden;
-		border-bottom: 2px solid #222;
-		border-top: 2px solid #222;
-		left: 0px;
-		width: 100%;
-		cursor: pointer;
 	}
 
 	.time-cursor {
